b0ed33f3b01cf1bbcdd3d39d1756482eeda29bd3
Moshe Zadka <zadka.moshe@gmail.com>
2015-10-30 20:32:23 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+# Copyright (c) Moshe Zadka
+# See LICENSE for details.
+import setuptools
+
+setuptools.setup(
+    url='https://github.com/moshez/luggage',
+    name='luggage',
+    packages=setuptools.find_packages(),
+    version='0.0.1',
+)
